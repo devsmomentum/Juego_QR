34cd4972b8dab36c9efb6bbc89eaffe277de9013
Andres <parraandres723@gmail.com>
2026-02-13 17:08:59 -0400

    ADD logic in build.gradle to could deploy for playstore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,16 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
 }
 
 android {
@@ -46,6 +54,41 @@ android {
 
     tasks.withType<JavaCompile> {
         options.compilerArgs.add("-Xlint:-options")
+    }
+
+    signingConfigs {
+        create("release") {
+            // Usamos .toString() para asegurar el tipo sin hacer "cast"
+            keyAlias = keystoreProperties["keyAlias"].toString()
+            keyPassword = keystoreProperties["keyPassword"].toString()
+            storePassword = keystoreProperties["storePassword"].toString()
+            
+            // Corregimos el error del 'it' siendo más explícitos
+            val path = keystoreProperties["storeFile"]?.toString()
+            if (path != null) {
+                storeFile = file(path)
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+    // Para quitar el aviso del jvmTarget (opcional pero recomendado)
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
