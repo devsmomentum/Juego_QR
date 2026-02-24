0ae36e85067405928873dfc7ba14605376b57680
Andres <parraandres723@gmail.com>
2026-02-23 21:46:37 -0400

    UPDATE max of clues

--- lib/features/admin/providers/event_creation_provider.dart
+++ lib/features/admin/providers/event_creation_provider.dart
@@ -218,7 +218,7 @@ class EventCreationProvider extends ChangeNotifier {
   }
 
   void setNumberOfClues(int value) {
-    if (value > 12) value = 12;
+    if (value > 20) value = 20;
     if (value < 0) value = 0;
     _numberOfClues = value;
     notifyListeners();
